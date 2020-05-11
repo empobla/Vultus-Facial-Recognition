7025522dadd8c07aeb28c87351dff46505dce921
Luis Antonio García García <31284430+WichoGarcia@users.noreply.github.com>
2020-05-11 16:26:58 -0500

    Update FaceDetection.cpp

--- Face_Detection/source/FaceDetection.cpp
+++ Face_Detection/source/FaceDetection.cpp
@@ -15,6 +15,17 @@ FaceDetector::FaceDetector(){
     flags = 0;
 }
 
+FaceDetector::FaceDetector(const string path){
+    faceCascade.load("../haarcascade_frontalface_alt.xml");
+    size = 30;
+    scale = 5.0;
+    window_scaling = 1.1; //This multiplies by the size to find the next bigger image, if there even is one to begin with 
+    minClassifiers = 4;
+    imgHeight = 30;
+    flags = 0;
+}
+    
+
 FaceDetector::FaceDetector(string faceCascadeFile, int scale, int size, double scale_factor, int minConsensus, int flag){
     faceCascade.load(faceCascadeFile);
     this->scale = scale;
