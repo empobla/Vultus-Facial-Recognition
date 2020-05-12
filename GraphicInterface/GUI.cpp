e2e1a6c36d3a885e7265b14ef6afa850494bfcf5
Carlos García <A01025948@itesm.mx>
2020-05-12 11:35:21 -0500

    Added a function to capture one frame with a button action.

--- GraphicInterface/GUI.cpp
+++ GraphicInterface/GUI.cpp
@@ -13,29 +13,23 @@ int main() {
     while (true) {
         frame = cv::Scalar(100, 100, 100);
         cvui::text(frame, 10, 10, "VULTUS software");
-        // Show a button at position (110, 80)
+
         if (cvui::button(frame, 500, 80, "Face verification")) { // To manually verify an unauthorized access
             //wFaceVerification.OpenWindow();
             count++;
             cout << count << endl;
             wOpener -> FaceVerificationWindow();
-            // The button was clicked, so let's increment our counter.
-
         }
         if (cvui::button(frame, 500, 160, "Face identification")) { // Detects the 10 closest matches to the faces database
             // The button was clicked, so let's increment our counter.
-
         }
         if (cvui::button(frame, 500, 240, "Enroll a student")) { // To feed the database with new values
             // The button was clicked, so let's increment our counter.
-
         }
         cvui::imshow(WINDOW1_NAME, frame);
         if (cv::waitKey(20) == 27) {
             break;
         }
     }
-
     return 0;
-
 }
--- GraphicInterface/Screens.cpp
+++ GraphicInterface/Screens.cpp
@@ -7,11 +7,21 @@ Screens::Screens() {
 
 }
 
+void CaptureFrame(int i, cv::Mat frame){
+    char filename[80];
+    sprintf(filename,"/root/workspace/GraphicInterface/test_%d.png",i);
+    imwrite(filename, frame);
+    i++;
+}
+
 void Screens::FaceVerificationWindow() {
     cvui::init(WINDOW2_NAME);
     cv::Mat frame = cv::Mat(cv::Size(1280, 720), CV_8UC3);
     int count = 0;
+    int i;
     while (true) {
+        i = 0;
+
         cv::VideoCapture cap;
         cap = cv::VideoCapture(0);
         cap.open(0);
@@ -27,15 +37,16 @@ void Screens::FaceVerificationWindow() {
             // Show a button at position (110, 80)
             if (cvui::button(frame, 500, 80, "Face verification")) {
                 // The button was clicked, so let's increment our counter.
+                CaptureFrame(i, frame);
             }
             if (cvui::button(frame, 500, 160, "Face identification")) {
                 // The button was clicked, so let's increment our counter.
-
+                CaptureFrame(i, frame);
             }
             if (cvui::button(frame, 500, 240, "Enroll a student")) {
                 // The button was clicked, so let's increment our counter.
-
             }
+            
             cv::imshow(WINDOW2_NAME, frame);
             if (cv::waitKey(20) == 27) {
                 break;
