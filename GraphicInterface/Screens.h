062e91d6137cd6ed635ca1413a40cf000d00d4b5
AlejandraNissan <ale.nissan@hotmail.com>
2020-05-14 10:27:55 -0500

    Added the correct directory and merged all the methods

---
+++ GraphicInterface/Screens.h
@@ -0,0 +1,27 @@
+#ifndef SCREENS_H
+#define SCREENS_H
+
+#include <opencv2/videoio.hpp>
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include "cvui.h"
+#include <list>
+#include <opencv2/face.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+class Screens{
+    public:
+    Screens();
+    void FaceVerificationWindow();
+    void FaceIdentificationWindow();
+    void EnrollStudentWindow();
+    void FaceIdentificationMethod(cv::Mat img);
+    void AprovedStudentIdentification(int aproved, cv::Mat dbImg1, cv::Mat dbImg2, cv::Mat dbImg3, cv::Mat dbImg4, cv::Mat dbImg5);
+    void FaceVerificationMethod(cv::Mat img, cv::String id);
+    void AprovedStudentVerification(int aproved, cv::Mat dbImg, cv::String id);
+    void confirmationFrame(std::string name, std::string age, std::string id, std::string image, int confirmation);
+};
+
+#endif
