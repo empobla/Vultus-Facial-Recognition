f15df8937a2faa75ef209f7ab5995f200c9e12ce
A01027265 <A010127265@itesm.mx>
2020-05-16 18:20:30 -0500

    Updated FaceDetection.hpp with comments

--- Face_Detection/source/FaceDetection.hpp
+++ Face_Detection/source/FaceDetection.hpp
@@ -1,14 +1,23 @@
-// This is the header file for the initial face detector program.
+/*******************************************
+ * Emilio Popovits : A01027265
+ * Sergio Hernandez : A01025210
+ * Luis Antonio Garcia : A01021865
+ * Eduardo Harari : A01025876
 
-#pragma once
+ This is the header file for the initial face detector program.
+ *******************************************/
+
+#pragma once	// Tells compiler to "copy" this only once, even if FaceDetection.hpp used more than once.
 
 #include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
 #include <string>
 #include <stdio.h>
 #include <vector>
 
 class FaceDetector {
-	private:
+private:
 	cv::CascadeClassifier faceCascade;
 	int scale;
 	int size;
@@ -17,10 +26,11 @@ class FaceDetector {
 	int imgHeight;
 	int flags;
 
-	public:
-	FaceDetector();
-	FaceDetector(const std::string path);
-	FaceDetector(std::string faceCascadeFile, int scale, int size, double scale_factor, int minConsensus, int flag);
-	std::vector<cv::Rect> detection(cv::Mat frame);
-	int getScale();
-};//Close FaceDetector class
+public:
+	FaceDetector();	// Default constructor, initializes variables with "default" values (set by us)
+	FaceDetector(const std::string path);	// Constructor that initializes faceCascade path to a chosen one
+	FaceDetector(std::string faceCascadeFile, int scale, int size, double scale_factor, int minConsensus, int flag);	// Overload constructor that initializes every other variable to user choice
+	std::vector<cv::Rect> detection(cv::Mat frame);	// Converts image into grayscale and resizes
+	std::vector<cv::Rect> identify(cv::Mat image);	// Detects faces and draws rectangles around them
+	int getScale();	// Getter method for scale variable
+};
