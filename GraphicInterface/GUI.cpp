7d833ead3f7d501c15ef89badeb4e601ecd4649f
AlejandraNissan <ale.nissan@hotmail.com>
2020-05-26 11:49:42 -0500

    SOme errors fixed

---
+++ GraphicInterface/GUI.cpp
@@ -0,0 +1,12 @@
+#include "Screens.h"
+//g++ *.cpp -o output `pkg-config --cflags --libs opencv4`
+
+
+using namespace std;
+
+int main() {
+    FaceRecognition *fr = new FaceRecognition(0.5);
+    Screens * wOpener = new Screens(fr);
+    wOpener->MainWindow();
+    return 0;
+}
